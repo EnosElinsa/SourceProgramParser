ae90106ef6dfb7ecb5cf61ffff02fb16f003ab10
Enos <1092903042@qq.com>
2022-10-29 20:37:29 +0800

    Implement the skeleton of the program
    UI and Main

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import ui.CommandLineUI;
+
+public class Main {
+    public static void main(String[] args) {
+        new CommandLineUI().run();
+    }
+}
---
+++ src/ui/CommandLineUI.java
@@ -0,0 +1,34 @@
+package ui;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CommandLineUI {
+    public void run() {
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("------------------------MENU------------------------\n1. Analyze the Source Program File in the Directory\n2. View the Analysis Results\n3. Exit\n----------------------------------------------------");
+                System.out.print(">Option: ");
+                int option = in.nextInt();
+                switch(option) {
+                    case 0: return;
+                    case 1:
+                        System.out.print(">Input a directory: ");
+                        String directory = in.next();
+                        File file = new File(directory);
+                        if (file.exists() == false || file.isDirectory() == false) {
+                            System.out.println(">Error: " + "[" + directory + "]" + "is not a directory or does not exit");
+                        }
+                        else {
+                            // anaylze();
+                        }
+                        break;
+                    case 2:
+                        // view the results
+                        break;
+                    default: System.out.println(">Invalid option!");
+                }
+            }
+        }
+    }
+}
